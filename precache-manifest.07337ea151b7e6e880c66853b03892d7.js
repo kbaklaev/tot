2718679a7d8629f10c4088e9e6f80204793df2e2
Konstantin Baklaev <kbaklaev@gmail.com>
2020-09-23 01:51:28 +0500

    Updates

---
+++ precache-manifest.07337ea151b7e6e880c66853b03892d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b4dc9fd9fe08c565794b41f01a3885",
+    "url": "/tot/index.html"
+  },
+  {
+    "revision": "61c0f92c42623618459e",
+    "url": "/tot/static/css/main.6c0d66fc.chunk.css"
+  },
+  {
+    "revision": "d45fc8dfd0dcb136f1a4",
+    "url": "/tot/static/js/2.a95e3432.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/tot/static/js/2.a95e3432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61c0f92c42623618459e",
+    "url": "/tot/static/js/main.affe4568.chunk.js"
+  },
+  {
+    "revision": "4a09c9dcf42424873b57",
+    "url": "/tot/static/js/runtime-main.7991cdfe.js"
+  }
+]);
